c2bab0b1ff6d620d3ac3750702936f93dec52580
VictorySky143 <cropotato143@gmail.com>
2025-08-10 12:01:29 +0800

    Add files via upload

--- LogInModule.py
+++ LogInModule.py
@@ -1,19 +1,29 @@
 from nicegui import ui as UI
 from HeaderModule import HeaderModule
+import mysql.connector
+
+
 class LogInModule:
-    def __init__(self):
-        # Wrapper row to center the login card
-        with UI.row().style('width: 100%; height: 100vh;').classes("justify-center items-center bg-blue-100"):
-            with UI.card().classes('p-8 shadow-lg bg-white rounded-lg w-96') as self.LogInInput:
-                UI.label("Log In").classes("text-2xl font-semibold text-blue-900 mb-4")
 
-                UI.input(label='Username or Email', placeholder='Enter your credentials') \
-                    .classes("w-full mb-4")
-
-                UI.input(label='Password', placeholder='Enter your password', password=True, password_toggle_button=True) \
-                    .classes("w-full mb-6")
-
-                self.LogInBtn = UI.button("Confirm", on_click=lambda: UI.notify("Logging in...")) \
-                    .classes("bg-blue-700 text-white w-full hover:bg-blue-800")
+	async def LogIn(self, input_name, input_pass):
+		mydb = mysql.connector.connect(host="localhost", user="root", password="", database = "Capstone")
+		mycursor = mydb.cursor()
+		sql = "SELECT Id FROM Credentials WHERE Name = %s AND Password = %s"
+		val = (input_name, input_pass)
+		mycursor.execute(sql, val)
+		result = mycursor.fetchall()
+		if result:
+			UI.notify("Logging In")
+			UI.navigate.to('/MainPage')
+		else:
+			UI.notify ("Wrong password/email")
+	def __init__(self):
+	
+		with UI.row().style('width: 100%; height: 100vh;').classes("justify-center items-center bg-blue-100"):
+			with UI.card().classes('p-8 shadow-lg bg-white rounded-lg w-96') as self.LogInInput:
+				UI.label("Log In").classes("text-2xl font-semibold text-blue-900 mb-4")
+				self.Name = UI.input(label='Username or Email', placeholder='Enter your credentials').classes("w-full mb-4")
+				self.Pass= UI.input(label='Password', placeholder='Enter your password', password=True, password_toggle_button=True).classes("w-full mb-6")
+				self.LogInBtn = UI.button("Confirm", on_click=lambda: LogIn(Name.value, Pass.value)).classes("bg-blue-700 text-white w-full hover:bg-blue-800")
 
 UI.run()
