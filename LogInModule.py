fdab6a68308006c287e0f0845340c679e6b8414f
AldrinMedina <158017731+AldrinMedina@users.noreply.github.com>
2025-08-15 21:59:38 +0800

    Update LogInModule.py

--- LogInModule.py
+++ LogInModule.py
@@ -6,27 +6,33 @@ import mysql.connector
 class LogInModule:
 
 	async def LogIn(self, input_name, input_pass):
-		mydb = mysql.connector.connect(host="localhost", user="root", password="", database = "Capstone")
+		mydb = mysql.connector.connect(
+		    host=os.getenv("MYSQLHOST"),
+		    user=os.getenv("MYSQLUSER"),
+		    password=os.getenv("MYSQLPASSWORD"),
+		    database=os.getenv("MYSQLDATABASE"),
+		    port=int(os.getenv("MYSQLPORT", 3306))
+		)
 		mycursor = mydb.cursor()
-		if input_name == "Joseph M.":
-			if input_pass == "12345":
-				UI.notify("Logging In")
-				UI.navigate.to('/MainPage')
-			else:
-				UI.notify("Wrong password.")
-		else:
-			UI.notify("No account found.")
+		#if input_name == "Joseph M.":
+		#	if input_pass == "12345":
+		#		UI.notify("Logging In")
+		#		UI.navigate.to('/MainPage')
+		#	else:
+		#		UI.notify("Wrong password.")
+		#else:
+		#	UI.notify("No account found.")
 		
 
-		#sql = "SELECT Id FROM Credentials WHERE Name = %s AND Password = %s"
-		#val = (input_name, input_pass)
-		#mycursor.execute(sql, val)
-		#result = mycursor.fetchall()
-		#if result:
-		#	UI.notify("Logging In")
-		#	UI.navigate.to('/MainPage')
-		#else:
-		#	UI.notify ("Wrong password/email")
+		sql = "SELECT Id FROM Credentials WHERE Name = %s AND Password = %s"
+		val = (input_name, input_pass)
+		mycursor.execute(sql, val)
+		result = mycursor.fetchall()
+		if result:
+			UI.notify("Logging In")
+			UI.navigate.to('/MainPage')
+		else:
+			UI.notify ("Wrong password/email")
 	def __init__(self):
 	
 		with UI.row().style('width: 100%; height: 100vh;').classes("justify-center items-center bg-blue-100"):
