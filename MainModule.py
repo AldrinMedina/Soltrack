ed7327ceaf0b146e62513ca5eb848ad211d05d00
VictorySky143 <cropotato143@gmail.com>
2025-09-13 15:58:58 +0800

    Update MainModule.py

--- MainModule.py
+++ MainModule.py
@@ -12,10 +12,10 @@ class MainModule:
 		infura_url = "https://sepolia.infura.io/v3/0a916ab0592e408d9c9bee7ff50a2fd6"
 		web3 = Web3(Web3.HTTPProvider(infura_url))
 		address1 = "0xEb8f64FDf7537086E9E3AA720c7e06217f62156F"
-	if web3.is_connected():
-		print("Connection to Sepolia RPC successful!")
-		balance_wei = web3.eth.get_balance(address)
-		balance_eth = web3.from_wei(balance_wei, 'ether')
-		print(f"Balance of {address}: {balance_eth} ETH")
-	else:
-    	print("Connection failed. Check your API key and network status.")
+		if web3.is_connected():
+			print("Connection to Sepolia RPC successful!")
+			balance_wei = web3.eth.get_balance(address)
+			balance_eth = web3.from_wei(balance_wei, 'ether')
+			print(f"Balance of {address}: {balance_eth} ETH")
+		else:
+    		print("Connection failed. Check your API key and network status.")
